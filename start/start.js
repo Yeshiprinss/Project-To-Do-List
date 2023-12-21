468f3a37ea738fc325217cbbf3243837e75146e4
Yeshiprinss <martinezyessenia068@gmail.com>
2023-12-21 14:18:16 +0100

    add style in js

--- start/start.js
+++ start/start.js
@@ -1,14 +1,3 @@
-function addTask() {
-    const input = document.getElementById('newTaskInput');
-    const taskList = document.getElementById('taskList');
-    const taskText = input.value.trim();
-  
-    if (taskText !== '') {
-      const li = document.createElement('li');
-      li.textContent = taskText;
-      taskList.appendChild(li);
-      input.value = '';
-    } else {
-      alert('Please enter a valid task.');
-    }
-  }
+function redirectToPage() {
+  window.location.href = 'nav.html';
+}
